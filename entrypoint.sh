6123d804ebad6cab9aee6ad0ff784247555e0d0f
Matthew Fala <falamatt@amazon.com>
2022-11-23 22:36:33 +0000

    sync-r3-keepaliveoff

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 echo -n "AWS for Fluent Bit Container Image Version "
 cat /AWS_FOR_FLUENT_BIT_VERSION
+export RAND=data-r3-1_9_10-keepalive-off-rand-$(($RANDOM%99999))$(($RANDOM%99999))$(($RANDOM%99999))
 exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -c /fluent-bit/etc/fluent-bit.conf
