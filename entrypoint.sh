299c59db73a0b556d99a758dc4e625b01801b249
Matthew Fala <falamatt@amazon.com>
2023-01-26 21:56:47 +0000

    aws: test sequence token deprecation control (no changes) 1 workers
    Signed-off-by: Matthew Fala <falamatt@amazon.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 echo -n "AWS for Fluent Bit Container Image Version "
 cat /AWS_FOR_FLUENT_BIT_VERSION
+export DATA_STREAM_NAME=data-std-control-1-workers
+export RAND=$(($RANDOM%99999))$(($RANDOM%99999))$(($RANDOM%99999))
+export N_WORKERS=1
 exec /fluent-bit/bin/fluent-bit -e /fluent-bit/firehose.so -e /fluent-bit/cloudwatch.so -e /fluent-bit/kinesis.so -c /fluent-bit/etc/fluent-bit.conf
